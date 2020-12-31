a886ede1bd76064800bfdf18da578f8a11a16e1f
shahd395 <76439179+shahd395@users.noreply.github.com>
2020-12-31 14:58:21 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -197,8 +197,8 @@ def runJob():
     
     #print('---------------------------------------------------------------')
     result = ''
-    if q1 = form.getvalue('q1')
-    q1_value = form.getvalue('1')
+    q1 = form.getvalue('q1')
+    if q1_value = form.getvalue('1')
     print('معافى')
     result = 'معافى'
     
